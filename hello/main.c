eb8b01276683f1c523dc285d568b421048e839a4
lusca <lucaspacheco747@gmail.com>
2025-06-03 20:28:11 -0300

    key_reader implementation

--- hello/main.c
+++ hello/main.c
@@ -6,17 +6,32 @@
  *
  *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
  *  and includes modifications made by the following author(s):
- *  ViniMacedo <vii-30@usp.br>
+ *  Lucas Pereira <pereira_lucas@usp.br>
  */
 
 #include <stdio.h>
 
+extern char key_reader(void);
+
 int main(void)   
 {
-  printf ("Hello World");
+  char key_pressed[2];
+  key_pressed[1] = '\0';
+
+  printf("Lendo Teclado\r\n");
+
+  for(;;){
+    key_pressed[0] = key_reader();
+    printf("Tecla ");
+    printf(key_pressed);
+    printf(" pressionada\r\n");
+
+    if(key_pressed[0] == 'q') break;
+  }
+  printf("Leitura encerrada\r");
+
   return 0;
 }
-
 
 
 
